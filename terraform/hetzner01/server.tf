cc1bc6d1f25885e6cf46f2e5001db7ce808db0b2
dylode <spam@geluidtijd.nl>
2023-05-19 17:42:35 +0200

    added hetzner

---
+++ terraform/hetzner01/server.tf
@@ -0,0 +1,58 @@
+resource "hcloud_server" "webserver" {
+  image        = "debian-11"
+  name         = "webserver-${count.index + 1}"
+  location     = "fsn1"
+  server_type  = "cx11"
+  ssh_keys     = ["dylan@dylan-mint"]
+  
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = false
+  }
+
+  count    = 2
+}
+
+resource "hcloud_server" "database" {
+  image        = "debian-11"
+  name         = "database-${count.index + 1}"
+  location     = "fsn1"
+  server_type  = "cx11"
+  ssh_keys     = ["dylan@dylan-mint"]
+  
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = false
+  }
+
+  count    = 3
+}
+
+resource "ansible_host" "webserver" {
+  for_each = {
+    for index, server in hcloud_server.webserver : index => server.ipv4_address
+  }
+
+  name   = each.value
+  groups = ["webserver", "monitored"]
+
+  variables = {
+    providerName = "hetzner01"
+    providerType = "hetzner"
+  }
+}
+
+resource "ansible_host" "database" {
+  for_each = {
+    for index, server in hcloud_server.database : index => server.ipv4_address
+  }
+
+  name   = each.value
+  groups = ["database", "monitored"]
+
+  variables = {
+    providerName = "hetzner01"
+    providerType = "hetzner"
+  }
+}
+
